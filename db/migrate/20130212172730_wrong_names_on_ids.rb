946ea4e5368736d0e80fb035b8c1cf5f03f50441
Zack Tillotson <zachery.tillotson@echostar.com>
2013-02-19 14:37:30 -0700

    Updated database information

--- db/migrate/20130212172730_wrong_names_on_ids.rb
+++
@@ -1,8 +0,0 @@
-class WrongNamesOnIds < ActiveRecord::Migration
-  def up
-    remove_column :lines, :line_player_id, :integer
-  end
-
-  def down
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 t = Team.create([{name: 'oneone'}])
-p = Player.create([{name: 'alice', team_id: t.first.id}, {name: 'bob', team_id: t.first.id}, {name: 'charlie', team_id: t.first.id}])
+p = Player.create([
+    {name: 'alice', team_id: t.first.id},
+   {name: 'bob', team_id: t.first.id},
+   {name: 'charlie', team_id: t.first.id},
+   {name: 'delta', team_id: t.first.id},
+   {name: 'echo', team_id: t.first.id},
+   {name: 'foxtrot', team_id: t.first.id},
+   {name: 'global', team_id: t.first.id},
+   {name: 'harry', team_id: t.first.id},
+   {name: 'incontrol', team_id: t.first.id},
+   {name: 'lemon', team_id: t.first.id},
+   {name: 'mortimer', team_id: t.first.id},
+   {name: 'nifty', team_id: t.first.id},
+   {name: 'oscar', team_id: t.first.id},
+   {name: 'prego', team_id: t.first.id},
+   {name: 'quickly', team_id: t.first.id},
+   {name: 'ricky', team_id: t.first.id},
+   {name: 'stacy', team_id: t.first.id},
+   {name: 'tilly', team_id: t.first.id},
+   {name: 'under', team_id: t.first.id},
+   {name: 'vergo', team_id: t.first.id}
+])
 g = Game.create([{description: 'game 1 vs other team', active: true, team_id: t.first.id}])
 l = Line.create([{scored: 0, active: 1, game_id: g.first.id}])
 Player.all.each { |pl| l.first.players << pl }
